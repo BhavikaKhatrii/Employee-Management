e5399c7e5581bc615eca89db7a72577072fa3a28
BHAVIKA BRAHMKHATRI <86822587+BhavikaKhatrii@users.noreply.github.com>
2022-05-14 07:31:34 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Create a Student Management System:
+a) Capability of adding Students
+b) Add Subject Name
+c) Generate Result Sheet (Add marks per subject and calculate grades)
+*/
+
+using System;
+
+namespace StudentManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Object creation of Application class
+            Application app = new Application();
+            app.ConsoleApp();
+            app.UserChoice();
+            #endregion
+        }
+    }
+}
